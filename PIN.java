3635b214b5c6b7ec4e8a2f54c0f8c44a0f334336
pearlyenh <pearlyhelia@gmail.com>
2026-08-10 08:15:30 +0800

    With Pin only

--- PIN.java
+++ PIN.java
@@ -1,70 +1,122 @@
 package ATM;
 import java.util.Scanner;
 
-class PIN{
-
+public class PIN {
     static Scanner scanner = new Scanner(System.in);
-    static String correctPin = "1234";
+
+    private static double balance = 10000.0;
+
+    static int attempts;
     static String pin;
-    static int attempt;
+    static String correctPin;
     static int option;
+    static double depositAmount;
+    static double add;
+    static double withdraw;
+    static int exit;
 
-    private double balance = 50000;
 
-    public static Boolean login(){
+    public static Boolean Login(){
+        attempts = 3;
 
-        attempt = 3;
-        
-        while(attempt > 0){
-            System.out.println("=======================");
-            System.out.println("       ATM SYSTEM      ");
-            System.out.println("=======================");
-            System.out.println("Enter PIN: ");
+        while (attempts > 0){
+            System.out.println("\n========== LOGIN ==========");
+            System.out.print("Enter PIN: ");
             pin = scanner.nextLine();
 
             if (pin.equals(correctPin)){
-                System.out.println("Login Successful!");
+                System.out.println("\nLogin Successful");
                 return true;
             }
             else {
-                attempt--;
-                
-                if (attempt > 0){
-                    System.out.println("Unsuccessful login. Please try again. ");
-                    System.out.println("Remaining attempt: " + attempt);
+                attempts--;
+
+                if (attempts > 0){
+                    System.out.println("\nInvalid PIN");
+                    System.out.println("Remaining attempts: " + attempts);
                 }
                 else {
-                    System.out.println("Account locked.");
+                    System.out.println("\nAccount Locked.");
                 }
             }
         }
         return false;
     }
     public static void MainMenu(){
-        System.out.println("========== ATM MENU ==========");
+        System.out.println("\n========== ATM MENU ==========");
         System.out.println("[1] Check Balance");
         System.out.println("[2] Deposit");
         System.out.println("[3] Withdraw");
-        System.out.println("[4] Change PIN");
-        System.out.println("[5] Logout");
-        System.out.println("==============================");
-        System.out.print("Choose an option:");
+        System.out.println("[4] Exit");
+        System.out.print("\nEnter an option: ");
         option = scanner.nextInt();
     }
-    public static void checkBalance(){
-        System.out.println("========== Checking of Balance ==========");
-        System.out.println("Current Balance: ");
+    public static void CheckBalance(){
+        System.out.println("\n========== CHECK BALANCE ===========");
+        System.out.println("Current Balance: " + balance);
     }
-    public static void deposit(){
-        System.out.println();
+    public static void Deposit(){
+        System.out.println("\n============ DEPOSIT ============");
+        System.out.print("Enter deposit amount: ");
+        depositAmount = scanner.nextDouble();
+
+        if (depositAmount > 0){
+            balance = depositAmount + balance;
+            System.out.println("\nDeposit Successful");
+            System.out.println("Updated balance: " + balance);
+        }
+        else {
+            System.out.println("\nInvalid deposit amount");
+        }
     }
-    public static void withdraw(){
-        System.out.println();
-    }
-    public static void changePin(){
-        System.out.println();
+    public static void Withdraw(){
+        System.out.println("\n============== WITHDRAW ===============");
+        System.out.print("Enter withdrawal amount: ");
+        withdraw = scanner.nextDouble();
+
+        if (withdraw > balance){
+            System.out.println("\nInvalid Withdrawal amount");
+        }
+        else {
+            balance = balance - withdraw;
+            System.out.println("\nWithdrawal Successful");
+            System.out.println("Updated Balance: " + balance);
+        }
     }
     public static void main(String[] args) {
-        
+        correctPin = "1234";
+
+        if (Login() == true){
+            while (true){
+                MainMenu();
+                if (option == 1){
+                    CheckBalance();
+                }
+                else if (option == 2){
+                    Deposit();
+                }
+                else if (option == 3){
+                    Withdraw();
+                }
+                else if (option == 4){
+                    System.out.println("\n Are you sure do you want to exit? ");
+                    System.out.println("[1] Yes");
+                    System.out.println("[2] No");
+                    System.out.print("Enter option: ");
+                    exit = scanner.nextInt();
+                    
+                    if (exit == 1){
+                        System.out.println("\nThank you. See you next time!");
+                        break;
+                    }
+                    else if (exit == 2){
+                        System.out.println("\nWELCOME BACK!");
+                    }
+                    else {
+                        System.out.println("\nInvalid input. Try again.");
+                    }
+                }
+            }
+        }  
     }
 }
